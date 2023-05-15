d1f7db9dff91c234eae92a2c5e7a5ad4c8dfcb5d
jacesleeman <46321222+jacesleeman@users.noreply.github.com>
2023-05-15 19:39:34 -0400

    First Push Nothing Here

---
+++ src/main/java/com/person98/extendedwhitelist/ExtendedWhitelist.java
@@ -0,0 +1,17 @@
+package com.person98.extendedwhitelist;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ExtendedWhitelist extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
