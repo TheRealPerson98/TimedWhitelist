caf96a85898ea93f89db0eda409f09baa24db0c5
jacesleeman <46321222+jacesleeman@users.noreply.github.com>
2023-05-16 15:32:12 -0400

    Link To Discord

--- src/main/java/com/person98/extendedwhitelist/managers/WhitelistManager.java
+++ src/main/java/com/person98/extendedwhitelist/managers/WhitelistManager.java
@@ -1,6 +1,12 @@
 package com.person98.extendedwhitelist.managers;
 
 import com.person98.extendedwhitelist.ExtendedWhitelist;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.MessageEmbed;
+import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
+import net.dv8tion.jda.api.requests.GatewayIntent;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.OfflinePlayer;
@@ -8,9 +14,12 @@ import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.scheduler.BukkitRunnable;
 
+import java.awt.*;
 import java.io.File;
 import java.io.IOException;
 import java.util.*;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 public class WhitelistManager {
@@ -22,7 +31,8 @@ public class WhitelistManager {
 
     private boolean kickOnWhitelistEnd;
     private boolean sendExpirationWarnings;
-
+    private JDA jda;
+    private String channelId;
     public WhitelistManager(ExtendedWhitelist plugin) {
         this.plugin = plugin;
         this.whitelistFile = new File(plugin.getDataFolder(), "whitelist.yml");
@@ -31,7 +41,18 @@ public class WhitelistManager {
 
         this.kickOnWhitelistEnd = config.getBoolean("kickOnWhitelistEnd", true);
         this.sendExpirationWarnings = config.getBoolean("sendExpirationWarnings", true);
+        if (config.getBoolean("discordIntegrationEnabled")) {
+            try {
+                jda = JDABuilder.createDefault(config.getString("discordBotToken"))
+                        .enableIntents(GatewayIntent.GUILD_MESSAGES)
+                        .build();
+                channelId = config.getString("discordChannelId");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
+
 
     public void addPlayerToWhitelist(UUID playerUUID, long durationInSeconds) {
         OfflinePlayer player = Bukkit.getOfflinePlayer(playerUUID);
@@ -48,7 +69,21 @@ public class WhitelistManager {
 
         task.runTaskLater(plugin, durationInSeconds * 20);
         tasks.put(playerUUID, task);
+
+        if (jda != null) {
+            TextChannel channel = jda.getTextChannelById(channelId);
+            if (channel != null) {
+                EmbedBuilder embedBuilder = new EmbedBuilder();
+                embedBuilder.setTitle("Whitelist Update");
+                embedBuilder.setDescription(player.getName() + " has been added to the whitelist for " + formatDuration(durationInSeconds) + ".");
+                MessageEmbed embed = embedBuilder.build();
+                embedBuilder.setColor(Color.GREEN); // Set the color to green (you can use other Color constants or specify a custom RGB value)
+
+                channel.sendMessageEmbeds(embed).queue();
+            }
+        }
     }
+
 
     public void addTimeToWhitelist(UUID playerUUID, long additionalTimeInSeconds) {
         OfflinePlayer player = Bukkit.getOfflinePlayer(playerUUID);
@@ -119,7 +154,22 @@ public class WhitelistManager {
         if (task != null) {
             task.cancel();
         }
+
+        if (jda != null) {
+            TextChannel channel = jda.getTextChannelById(channelId);
+            if (channel != null) {
+                EmbedBuilder embedBuilder = new EmbedBuilder();
+                embedBuilder.setTitle("Whitelist Update");
+                embedBuilder.setDescription(player.getName() + "'s whitelist has expired.");
+                MessageEmbed embed = embedBuilder.build();
+                embedBuilder.setColor(Color.RED); // Set the color to green (you can use other Color constants or specify a custom RGB value)
+
+                channel.sendMessageEmbeds(embed).queue();
+            }
+        }
     }
+
+
 
     public long getTimeLeft(UUID playerUUID) {
         OfflinePlayer player = Bukkit.getOfflinePlayer(playerUUID);
@@ -209,6 +259,27 @@ public class WhitelistManager {
                 }
             }, ticksOneMinute);
         }
+    }
+    public String formatDuration(long durationInSeconds) {
+        long days = TimeUnit.SECONDS.toDays(durationInSeconds);
+        long hours = TimeUnit.SECONDS.toHours(durationInSeconds) - TimeUnit.DAYS.toHours(days);
+        long minutes = TimeUnit.SECONDS.toMinutes(durationInSeconds) - TimeUnit.DAYS.toMinutes(days) - TimeUnit.HOURS.toMinutes(hours);
+        long seconds = TimeUnit.SECONDS.toSeconds(durationInSeconds) - TimeUnit.DAYS.toSeconds(days) - TimeUnit.HOURS.toSeconds(hours) - TimeUnit.MINUTES.toSeconds(minutes);
+
+        StringBuilder sb = new StringBuilder();
+        if (days > 0) {
+            sb.append(days).append(" days ");
+        }
+        if (hours > 0) {
+            sb.append(hours).append(" hours ");
+        }
+        if (minutes > 0) {
+            sb.append(minutes).append(" minutes ");
+        }
+        if (seconds > 0) {
+            sb.append(seconds).append(" seconds");
+        }
+        return sb.toString().trim();
     }
 
 }
